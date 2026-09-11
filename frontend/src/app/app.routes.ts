70cf2039d8ac24f1f9bea2260c4d3add833002dc
Masa Gavrilovic <masagavrilovic13@gmail.com>
2026-09-11 02:20:30 +0200

    feat: add landing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { Login } from './auth/components/login/login';
+import { Register } from './auth/components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPage
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+        {
+        path: 'register',
+        component: Register
+    },
+];
---
+++ frontend/src/app/landing-page/landing-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './landing-page.html',
+})
+export class LandingPage {}
