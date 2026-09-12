1510c4c77ae849be1e9b3de72d10c6c9798c978d
Masa Gavrilovic <masagavrilovic13@gmail.com>
2026-09-12 17:37:54 +0200

    feat: implement login page UI and validation

--- frontend/src/app/auth/components/login/login.ts
+++ frontend/src/app/auth/components/login/login.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  protected readonly isSubmitting = signal(false);
+  protected readonly errorMessage = signal('');
+  
+  private readonly formBuilder = inject(FormBuilder);
+  protected readonly loginForm = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit()
+  {
+    if (this.loginForm.invalid || this.isSubmitting()) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isSubmitting.set(true);
+    this.errorMessage.set('');
+  }
+}
