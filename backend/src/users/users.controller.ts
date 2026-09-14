063c48f556d51cc56320e6c0023c6f6381ad9485
Masa Gavrilovic <masagavrilovic13@gmail.com>
2026-09-14 20:31:19 +0200

    feat: add GET /users/me endpoint

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  currentUser(@Req() req: any) {
+    return this.usersService.findOne(req.user.id)
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
