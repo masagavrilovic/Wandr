02491a9046a699956e876a4c20579430b8dfd50b
Masa Gavrilovic <masagavrilovic13@gmail.com>
2026-09-14 18:50:02 +0200

    feat: add page header layout

---
+++ frontend/src/app/shared/components/topbar/topbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './topbar.html'
+})
+export class Topbar {}
