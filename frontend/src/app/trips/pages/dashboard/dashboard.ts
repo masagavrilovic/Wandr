8912f13a784e16a6d735691312ce6363fd097455
Masa Gavrilovic <masagavrilovic13@gmail.com>
2026-09-15 17:55:25 +0200

    feat: add join trip modal layout

--- frontend/src/app/trips/pages/dashboard/dashboard.ts
+++ frontend/src/app/trips/pages/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TripCardList } from '../../components/trip-card-list/trip-card-list';
 import { Store } from '@ngrx/store';
 import { selectTripsTotal } from '../../store/trips.selectors';
@@ -17,7 +17,22 @@ export class Dashboard implements OnInit {
   private store = inject(Store);
   tripsCount$ = this.store.select(selectTripsTotal);
 
+  isJoinModalOpen = signal(false);
+  isSubmitting = signal(false);
   ngOnInit() {
       this.store.dispatch(loadTrips());
+  }
+
+  openJoinModal() {
+    this.isJoinModalOpen.set(true);
+  }
+
+  closeJoinModal() {
+    this.isJoinModalOpen.set(false);
+  }
+
+  onSubmit() {
+    this.isSubmitting.set(true);
+    this.closeJoinModal();
   }
 }
