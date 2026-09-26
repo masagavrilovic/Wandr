39bfd37e894e0e6d60847c5c0cc1e0d0ad02b9e9
Masa Gavrilovic <masagavrilovic13@gmail.com>
2026-09-26 17:26:18 +0200

    feat: add user summary dto

--- backend/src/user/dto/user-response.dto.ts
+++ backend/src/user/dto/user-response.dto.ts
@@ -13,3 +13,13 @@ export class UserResponseDto {
         this.email = user.email;
     }
 }
+
+export class UserSummaryDto {
+    firstName: string;
+    lastName: string;
+
+    constructor(user: User) {
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+    }
+}
